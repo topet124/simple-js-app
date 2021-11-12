5eee26648a2d7de0078d23e06a64847f09e3585d
Temitope segun Sule <57067649+topet124@users.noreply.github.com>
2021-11-12 05:35:18 +0200

    Added new pokemon objects

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-alert('Hello world');
-let favoriteFood = "rice";
 // declaring a variable
-document.write(favoriteFood);
+let pokemonList = [{
+    name: 'Bulbasaur',
+    height: 8,
+    type: ['psychic', 'ability']
+  },
+  {
+    name: 'Ivysaur',
+    height: 1,
+    type: ['chlorophyll', 'overgrow']
+  },
+  {
+    name: 'Charmeleon',
+    height: 1.1,
+    type: ['blaze', 'solarPower']
+  }
+];
+document.write(pokemonList[0].type[0])

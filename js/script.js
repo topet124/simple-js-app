88fad7ecdade7e2a891728f5acf590d929080f21
Temitope segun Sule <57067649+topet124@users.noreply.github.com>
2021-11-22 08:49:55 +0200

    refactored the code and added the IIFE function

--- js/script.js
+++ js/script.js
@@ -1,26 +1,51 @@
-// declaring a variable
-let pokemonList = [{
-    name: 'Bulbasaur',
-    height: 8,
-    type: ['psychic', 'ability']
-  },
-  {
-    name: 'Ivysaur',
-    height: 1,
-    type: ['chlorophyll', 'overgrow']
-  },
-  {
-    name: 'Charmeleon',
-    height: 1.1,
-    type: ['blaze', 'solarPower']
-  }
-];
-// looping through the object
-for (i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 5) {
-    document.write(pokemonList[i].name + ' ' + 'height:' + pokemonList[i].height + ' ' + '--Wow this is huge' + '<br>')
-  } else {
-    document.write(pokemonList[i].name + ' ' + 'height:' + pokemonList[i].height + ' ' + '<br>')
+   // invoking iife function
+   let pokemonRepo = (function() {
+     let pokemonList = [{
+         name: 'Bulbasaur',
+         height: 8,
+         type: ['psychic', 'ability']
+       },
+       {
+         name: 'Ivysaur',
+         height: 1,
+         type: ['chlorophyll', 'overgrow']
+       },
+       {
+         name: 'Charmeleon',
+         height: 1.1,
+         type: ['blaze', 'solarPower']
+       }
+     ];
 
-  }
-}
+     function add(pokemon) {
+       if (Object.keys(pokemon).length === 3) {
+         pokemonList.push(pokemon);
+       } else {
+         alert('you have to add three keys')
+       }
+     }
+
+     function getAll() {
+       return pokemonList;
+     }
+
+     return {
+       add: add,
+       getAll: getAll
+     };
+   })();
+
+   // add new element
+   pokemonRepo.add({
+     name: 'ashuar',
+     height: 10,
+     type: ['vision', 'sighted']
+   });
+   // looping through the object
+   pokemonRepo.getAll().forEach(function(item) {
+     if (item.height > 5) {
+       document.write(item.name + ' ' + 'height:' + item.height + ' ' + '--Wow this is huge' + '<br>')
+     } else {
+       document.write(item.name + ' ' + 'height:' + item.height + ' ' + '<br>')
+     }
+   });
